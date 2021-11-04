48e5843662d5141b3eefefccba28aec10ef34587
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2021-11-04 16:33:14 +0100

    build: without state design

--- main.go
+++ main.go
@@ -10,12 +10,15 @@ import (
 var VIDEO_STATE string = "UPLOADING"
 var WAIT_SECOND time.Duration = 10
 
+
+var video Video
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Video state : %s", VIDEO_STATE)
+    fmt.Fprintf(w, "Video : %s", video)
 }
 
 func handlerAsAuthor(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "(As Author) Video state : %s", VIDEO_STATE)
+    fmt.Fprintf(w, "(As Author) Video : %s", video)
 }
 
 func manageVideoState() {
@@ -26,8 +29,16 @@ func manageVideoState() {
 }
 
 func main() {
-		go manageVideoState()
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/author", handlerAsAuthor)
-    log.Fatal(http.ListenAndServe(":2375", nil))
+	video.name = "Le Bitcoin n'apporte rien de nouveau, change my mind"
+	video.author = "Léo"
+
+	go manageVideoState()
+  http.HandleFunc("/", handler)
+  http.HandleFunc("/iamleo", handlerAsAuthor)
+  log.Fatal(http.ListenAndServe(":2375", nil))
 }
+
+/*
+ TODOs :
+ - Add video struct : name, author
+	*/
---
+++ video.go
@@ -0,0 +1,6 @@
+package main
+
+type Video struct {
+	name string
+	author string
+}
